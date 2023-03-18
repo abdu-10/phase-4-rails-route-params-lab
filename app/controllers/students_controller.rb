b33114858835d9778afca36262227a7fc1cbc945
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-03-18 11:50:15 +0300

    commit tree b559175eab2e3eeb48096b28b567b9dd763d0d60

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  # def name
+  #   if (Student.find(params[:name]))
+  #     render json: Student.find(params[:name])      
+  #   else
+  #     render {err: "not_found"}
+  #   end
+  # end
 end
